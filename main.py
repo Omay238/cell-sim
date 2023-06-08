a55420348932d34d31457012a76ea656d864cfb9
Omay238 <238maculo238@gmail.com>
2023-06-08 15:53:10 -0600

    pygame works good

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ import pygame
 
 def main():
     print("Running Cell Sim v2.0.0")
+    pygame.init()
+    screen = pygame.display.set_mode((720, 720))
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        screen.fill("black")
+
+        # render
+
+        # end render
+
+        # logic
+
+        # end logic
+
+        pygame.display.flip()
+
+        clock.tick(60)
 
 
 if __name__ == "__main__":
